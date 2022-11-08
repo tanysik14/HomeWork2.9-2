e8ef62c0242b41e9b705305c9729b7a94ad1fc7c
HelloWorld <tanysik14>
2022-11-08 15:26:18 +0300

    Initial Commit

---
+++ HomeWork2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2.3
+//
+//  Created by Tatsiana Pauliukova on 8.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
