717c3153d7529229e0ecf2bc0666c2484d2b29ef
HelloWorld <tanysik14>
2022-11-08 22:14:31 +0300

    ready corrected homework, which I'm really high on)))))

--- HomeWork2.3/ViewController.swift
+++ HomeWork2.3/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // MARK: - IB Outlet
     
     @IBOutlet var viewColor: UIView!
@@ -27,11 +27,11 @@ class ViewController: UIViewController {
     // MARK: - IB Action
     override func viewDidLoad() {
         super.viewDidLoad()
-    viewColor.layer.cornerRadius = 10
-//        paintingView()
+        viewColor.layer.cornerRadius = 10
+        
         setupSlider()
         sliderAction()
-     
+        
     }
     
     
@@ -41,34 +41,20 @@ class ViewController: UIViewController {
         numberBlueColor.text = String(format: "%.2f", (sliderBlue.value))
         numberGreenColor.text = String(format: "%.2f", (sliderGreen.value))
         
+        viewColor.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
     }
-   
     
-//    @IBAction func paintingView() {
-//        var red = sliderRed.value
-//        var blue = sliderBlue.value
-//        var green = sliderGreen.value
-//     
-//        
-//        viewColor.backgroundColor = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: CGFloat(red))
-//    }
-   
-
-// MARK: - private setup
+    
+    
+    // MARK: - private setup
     private func setupSlider() {
         sliderRed.value = 0
-        sliderRed.minimumValue = 0
-        sliderRed.maximumValue = 1
         sliderRed.minimumTrackTintColor = .red
         sliderGreen.value = 0
-        sliderGreen.minimumValue = 0
-        sliderGreen.maximumValue = 1
         sliderGreen.minimumTrackTintColor = .green
         sliderBlue.value = 0
-        sliderBlue.minimumValue = 0
-        sliderBlue.maximumValue = 1
         sliderBlue.minimumTrackTintColor = .blue
-                                    
     }
+    
 }
 
